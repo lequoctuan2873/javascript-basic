78ba971a27be5d8be5faae22ae715f240d9cd6b0
lequoctuan2873 <lequoctuan444@gmail.com>
2024-04-24 18:32:52 +0700

    video chapter 3 video 2

--- video11/video11.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello world from HTML')
-
-let arr =['Mu', 'ManCity', 'Liver', 'Chealse']
-for(let i = 0; i < arr.length; i++){
-    console.log('check variable i =', arr[i])
-
-}
---
+++ video12/video12.js
@@ -0,0 +1,11 @@
+console.log('Hello world from HTML')
+let arrTop4 = ['ManCity', 'Liverpool','Mu', 'Chealse']
+
+// for(let i = 0; i < arrTop4.length; i++){
+//     console.log('Top',i+1,arrTop4[i])
+// }
+let i = 0
+while(i < arrTop4.length){
+    console.log('Top', i+1,arrTop4[i])
+    i++
+}
